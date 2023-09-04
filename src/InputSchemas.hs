9c6c7dc6c786be63a77033f0d1b2ad5bc961555c
harryprayiv <harryprayiv@gmail.com>
2023-09-04 19:26:00 -0400

    schema is fixed.  still stuck

--- src/InputSchemas.hs
+++ src/InputSchemas.hs
@@ -1,290 +1,254 @@
-{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DataKinds #-}
 
 module InputSchemas
-    ( flattenGameData
+    ( Boxscore
     ) where
-import Data.Aeson (eitherDecodeFileStrict)
+
+
+import Data.Aeson (eitherDecodeFileStrict, toJSON)
 import Data.Aeson.Schema
 import Data.HashMap.Strict (union, fromList)
 import qualified Data.Text as T
-import qualified Data.Map as Map
 import Data.Maybe (fromMaybe, catMaybes)
-import Data.Text (Text)
+import qualified Data.Map as Map
 
-type GameStats = [schema|
+type Boxscore = [schema|
   {
-    teams:   {
-      away:   {
-        players: List Player
+    teams: {
+      away: {
+        players: { 
+          value: {
+            person: List {
+              id: Int,
+              fullName: Text,
+            },
+            parentTeamId: Int,
+            allPositions: Maybe List {
+              code: Text,
+            },
+            status: List {
+              code: Text,
+            },
+            stats: {
+              batting: Maybe List {
+                gamesPlayed: Int,
+                flyOuts: Int,
+                groundOuts: Int,
+                runs: Int,
+                doubles: Int,
+                triples: Int,
+                homeRuns: Int,
+                strikeOuts: Int,
+                baseOnBalls: Int,
+                intentionalWalks: Int,
+                hits: Int,
+                hitByPitch: Int,
+                atBats: Int,
+                caughtStealing: Int,
+                stolenBases: Int,
+                groundIntoDoublePlay: Int,
+                groundIntoTriplePlay: Int,
+                plateAppearances: Int,
+                totalBases: Int,
+                rbi: Int,
+                leftOnBase: Int,
+                sacBunts: Int,
+                sacFlies: Int,
+                catchersInterference: Int,
+                pickoffs: Int,
+              },
+              pitching: Maybe List {
+                gamesPlayed: Int,
+                gamesStarted: Int,
+                groundOuts: Int,
+                airOuts: Int,
+                runs: Int,
+                doubles: Int,
+                triples: Int,
+                homeRuns: Int,
+                strikeOuts: Int,
+                baseOnBalls: Int,
+                intentionalWalks: Int,
+                hits: Int,
+                hitByPitch: Int,
+                atBats: Int,
+                caughtStealing: Int,
+                stolenBases: Int,
+                numberOfPitches: Int,
+                inningsPitched: Text,
+                wins: Int,
+                losses: Int,
+                saves: Int,
+                saveOpportunities: Int,
+                holds: Int,
+                blownSaves: Int,
+                earnedRuns: Int,
+                battersFaced: Int,
+                outs: Int,
+                gamesPitched: Int,
+                completeGames: Int,
+                shutouts: Int,
+                pitchesThrown: Int,
+                balls: Int,
+                strikes: Int,
+                hitBatsmen: Int,
+                balks: Int,
+                wildPitches: Int,
+                pickoffs: Int,
+                gamesFinished: Int,
+                inheritedRunners: Int,
+                inheritedRunnersScored: Int,
+                catchersInterference: Int,
+                sacBunts: Int,
+                sacFlies: Int,
+                passedBall: Int,
+              }
+            }
+          }
+        }
       },
-      home:   {
-        players: List Player
+      home: {
+        players: {
+          value: {
+            person: List {
+              id: Int,
+              fullName: Text,
+            },
+            parentTeamId: Int,
+            allPositions: Maybe List {
+              code: Text,
+            },
+            status: List {
+              code: Text,
+            },
+            stats: {
+              batting: Maybe List {
+                gamesPlayed: Int,
+                flyOuts: Int,
+                groundOuts: Int,
+                runs: Int,
+                doubles: Int,
+                triples: Int,
+                homeRuns: Int,
+                strikeOuts: Int,
+                baseOnBalls: Int,
+                intentionalWalks: Int,
+                hits: Int,
+                hitByPitch: Int,
+                atBats: Int,
+                caughtStealing: Int,
+                stolenBases: Int,
+                groundIntoDoublePlay: Int,
+                groundIntoTriplePlay: Int,
+                plateAppearances: Int,
+                totalBases: Int,
+                rbi: Int,
+                leftOnBase: Int,
+                sacBunts: Int,
+                sacFlies: Int,
+                catchersInterference: Int,
+                pickoffs: Int,
+              },
+              pitching: Maybe List {
+                gamesPlayed: Int,
+                gamesStarted: Int,
+                groundOuts: Int,
+                airOuts: Int,
+                runs: Int,
+                doubles: Int,
+                triples: Int,
+                homeRuns: Int,
+                strikeOuts: Int,
+                baseOnBalls: Int,
+                intentionalWalks: Int,
+                hits: Int,
+                hitByPitch: Int,
+                atBats: Int,
+                caughtStealing: Int,
+                stolenBases: Int,
+                numberOfPitches: Int,
+                inningsPitched: Text,
+                wins: Int,
+                losses: Int,
+                saves: Int,
+                saveOpportunities: Int,
+                holds: Int,
+                blownSaves: Int,
+                earnedRuns: Int,
+                battersFaced: Int,
+                outs: Int,
+                gamesPitched: Int,
+                completeGames: Int,
+                shutouts: Int,
+                pitchesThrown: Int,
+                balls: Int,
+                strikes: Int,
+                hitBatsmen: Int,
+                balks: Int,
+                wildPitches: Int,
+                pickoffs: Int,
+                gamesFinished: Int,
+                inheritedRunners: Int,
+                inheritedRunnersScored: Int,
+                catchersInterference: Int,
+                sacBunts: Int,
+                sacFlies: Int,
+                passedBall: Int,
+              }
+            }
+          }
+        }
       } 
-    } 
-  }
-|]
-
-type Player = [schema|
-  {
-    person: {
-      id: Int,
-      fullName: Text
-    },
-    parentTeamId: Int,
-    allPositions: List {
-      code: Text
-    },
-    status: {
-      code: Text
-    },
-    stats:   {
-      batting: Maybe BattingStats,
-      pitching: Maybe PitchingStats
-    },
-    seasonStats:   {
-      batting: Maybe SeasonBattingStats,
-      pitching: Maybe SeasonPitchingStats
     }
   }
 |]
 
-type BattingStats = [schema|
-  {
-    gamesPlayed: Int,
-    flyOuts: Int,
-    groundOuts: Int,
-    runs: Int,
-    doubles: Int,
-    triples: Int,
-    homeRuns: Int,
-    strikeOuts: Int,
-    baseOnBalls: Int,
-    intentionalWalks: Int,
-    hits: Int,
-    hitByPitch: Int,
-    atBats: Int,
-    caughtStealing: Int,
-    stolenBases: Int,
-    groundIntoDoublePlay: Int,
-    groundIntoTriplePlay: Int,
-    plateAppearances: Int,
-    totalBases: Int,
-    rbi: Int,
-    leftOnBase: Int,
-    sacBunts: Int,
-    sacFlies: Int,
-    catchersInterference: Int,
-    pickoffs: Int,
-    note: Text,
-    summary: Text,
-    stolenBasePercentage: Text,
-    atBatsPerHomeRun: Text
-  }
-|]
+-- flattenGameData :: Object GameStats -> Int -> Map.Map Int Player
+-- flattenGameData gameData gameId = 
+--   let
+--     awayPlayers = [get| gameData.teams.away.players[] |]
+--     homePlayers = [get| gameData.teams.home.players[] |]
+--     allPlayers = awayPlayers ++ homePlayers
 
-type PitchingStats = [schema|
-  {
-    gamesPlayed: Int,
-    gamesStarted: Int,
-    groundOuts: Int,
-    airOuts: Int,
-    runs: Int,
-    doubles: Int,
-    triples: Int,
-    homeRuns: Int,
-    strikeOuts: Int,
-    baseOnBalls: Int,
-    intentionalWalks: Int,
-    hits: Int,
-    hitByPitch: Int,
-    atBats: Int,
-    caughtStealing: Int,
-    stolenBases: Int,
-    numberOfPitches: Int,
-    inningsPitched: Text,
-    wins: Int,
-    losses: Int,
-    saves: Int,
-    saveOpportunities: Int,
-    holds: Int,
-    blownSaves: Int,
-    earnedRuns: Int,
-    battersFaced: Int,
-    outs: Int,
-    gamesPitched: Int,
-    completeGames: Int,
-    shutouts: Int,
-    pitchesThrown: Int,
-    balls: Int,
-    strikes: Int,
-    hitBatsmen: Int,
-    balks: Int,
-    wildPitches: Int,
-    pickoffs: Int,
-    gamesFinished: Int,
-    inheritedRunners: Int,
-    inheritedRunnersScored: Int,
-    catchersInterference: Int,
-    sacBunts: Int,
-    sacFlies: Int,
-    passedBall: Int,
-    note: Text,
-    summary: Text,
-    stolenBasePercentage: Text,
-    strikePercentage: Text,
-    homeRunsPer9: Text,
-    runsScoredPer9: Text
-  }
-|]
+--     transformPlayer :: Player -> Maybe (Int, Player)
+--     transformPlayer player 
+--       | null (fromMaybe [] $ playerAllPositions player) = Nothing
+--       | otherwise = 
+--         let
+--             playerId = playerPersonId $ playerPerson player
+--             positions = map ((read . T.unpack) . allPositionsCode) (playerAllPositions player)
+--             battingStats = fmap delFieldsBatting (playerStatsBatting $ playerStats player)
+--             pitchingStats = fmap delFieldsPitching (playerStatsPitching $ playerStats player)
+--             statsForGame = StatsType battingStats pitchingStats
+--             newPlayer = [put|
+--             {
+--                 person: {
+--                     id: ${playerPersonId $ playerPerson player},
+--                     fullName: ${playerPersonFullName $ playerPerson player}
+--                 },
+--                 parentTeamId: ${playerParentTeamId player},
+--                 allPositions: ${map (\pos -> [put| { code: ${allPositionsCode pos} } |]) (playerAllPositions player)},
+--                 status: {
+--                     code: ${statusCode $ playerStatus player}
+--                 },
+--                 stats: {
+--                     batting: ${fmap delFieldsBatting (playerStatsBatting $ playerStats player)},
+--                     pitching: ${fmap delFieldsPitching (playerStatsPitching $ playerStats player)}
+--                 },
+--                 seasonStats: {
+--                     batting: ${playerSeasonStatsBatting $ playerSeasonStats player},
+--                     pitching: ${playerSeasonStatsPitching $ playerSeasonStats player}
+--                 }
+--             }
+--             |]
+--         in Just (playerId, newPlayer)
 
-type SeasonBattingStats = [schema|
-  {
-    gamesPlayed: Int,
-    flyOuts: Int,
-    groundOuts: Int,
-    runs: Int,
-    doubles: Int,
-    triples: Int,
-    homeRuns: Int,
-    strikeOuts: Int,
-    baseOnBalls: Int,
-    intentionalWalks: Int,
-    hits: Int,
-    hitByPitch: Int,
-    avg: Text,
-    atBats: Int,
-    obp: Text,
-    slg: Text,
-    ops: Text,
-    caughtStealing: Int,
-    stolenBases: Int,
-    stolenBasePercentage: Text,
-    groundIntoDoublePlay: Int,
-    groundIntoTriplePlay: Int,
-    plateAppearances: Int,
-    totalBases: Int,
-    rbi: Int,
-    leftOnBase: Int,
-    sacBunts: Int,
-    sacFlies: Int,
-    babip: Text,
-    catchersInterference: Int,
-    pickoffs: Int,
-    atBatsPerHomeRun: Text
-  }
-|]
+--     delFieldsBatting :: BattingStats -> BattingStats
+--     delFieldsBatting stats = stats { note = "", summary = "", stolenBasePercentage = "", atBatsPerHomeRun = "" }
 
-type SeasonPitchingStats = [schema|
-  {
-    gamesPlayed: Int,
-    gamesStarted: Int,
-    flyOuts: Int,
-    groundOuts: Int,
-    airOuts: Int,
-    runs: Int,
-    doubles: Int,
-    triples: Int,
-    homeRuns: Int,
-    strikeOuts: Int,
-    baseOnBalls: Int,
-    intentionalWalks: Int,
-    hits: Int,
-    hitByPitch: Int,
-    atBats: Int,
-    obp: Text,
-    caughtStealing: Int,
-    stolenBases: Int,
-    stolenBasePercentage: Text,
-    numberOfPitches: Int,
-    era: Text,
-    inningsPitched: Text,
-    wins: Int,
-    losses: Int,
-    saves: Int,
-    saveOpportunities: Int,
-    holds: Int,
-    blownSaves: Int,
-    earnedRuns: Int,
-    whip: Text,
-    battersFaced: Int,
-    outs: Int,
-    gamesPitched: Int,
-    completeGames: Int,
-    shutouts: Int,
-    pitchesThrown: Int,
-    balls: Int,
-    strikes: Int,
-    strikePercentage: Text,
-    hitBatsmen: Int,
-    balks: Int,
-    wildPitches: Int,
-    pickoffs: Int,
-    groundOutsToAirouts: Text,
-    rbi: Int,
-    winPercentage: Text,
-    pitchesPerInning: Text,
-    gamesFinished: Int,
-    strikeoutWalkRatio: Text,
-    strikeoutsPer9Inn: Text,
-    walksPer9Inn: Text,
-    hitsPer9Inn: Text,
-    runsScoredPer9: Text,
-    homeRunsPer9: Text,
-    inheritedRunners: Int,
-    inheritedRunnersScored: Int,
-    catchersInterference: Int,
-    sacBunts: Int,
-    sacFlies: Int,
-    passedBall: Int
-  }
-|]
-
-flattenGameData :: Object GameStats -> Int -> Map.Map Int Player
-flattenGameData gameData gameId = 
-  let
-    awayPlayers = [get| gameData.teams.away.players[] |]
-    homePlayers = [get| gameData.teams.home.players[] |]
-    allPlayers = awayPlayers ++ homePlayers
-
-    transformPlayer :: Player -> Maybe (Int, Player)
-    transformPlayer player 
-      | null (fromMaybe [] $ playerAllPositions player) = Nothing
-      | otherwise = 
-        let
-            playerId = playerPersonId $ playerPerson player
-            positions = map ((read . T.unpack) . allPositionsCode) (playerAllPositions player)
-            battingStats = fmap delFieldsBatting (playerStatsBatting $ playerStats player)
-            pitchingStats = fmap delFieldsPitching (playerStatsPitching $ playerStats player)
-            statsForGame = StatsType battingStats pitchingStats
-            newPlayer = [put|
-            {
-                person: {
-                    id: ${playerPersonId $ playerPerson player},
-                    fullName: ${playerPersonFullName $ playerPerson player}
-                },
-                parentTeamId: ${playerParentTeamId player},
-                allPositions: ${map (\pos -> [put| { code: ${allPositionsCode pos} } |]) (playerAllPositions player)},
-                status: {
-                    code: ${statusCode $ playerStatus player}
-                },
-                stats: {
-                    batting: ${fmap delFieldsBatting (playerStatsBatting $ playerStats player)},
-                    pitching: ${fmap delFieldsPitching (playerStatsPitching $ playerStats player)}
-                },
-                seasonStats: {
-                    batting: ${playerSeasonStatsBatting $ playerSeasonStats player},
-                    pitching: ${playerSeasonStatsPitching $ playerSeasonStats player}
-                }
-            }
-            |]
-        in Just (playerId, newPlayer)
-
-    delFieldsBatting :: BattingStats -> BattingStats
-    delFieldsBatting stats = stats { note = "", summary = "", stolenBasePercentage = "", atBatsPerHomeRun = "" }
-
-    delFieldsPitching :: PitchingStats -> PitchingStats
-    delFieldsPitching stats = stats { note = "", summary = "", stolenBasePercentage = "", strikePercentage = "", homeRunsPer9 = "", runsScoredPer9 = "" }
-  in
-    Map.fromList $ catMaybes $ mapMaybe transformPlayer allPlayers
+--     delFieldsPitching :: PitchingStats -> PitchingStats
+--     delFieldsPitching stats = stats { note = "", summary = "", stolenBasePercentage = "", strikePercentage = "", homeRunsPer9 = "", runsScoredPer9 = "" }
+--   in
+--     Map.fromList $ catMaybes $ mapMaybe transformPlayer allPlayers
--- src/Main.hs
+++ src/Main.hs
@@ -10,122 +10,10 @@ import Data.Aeson (eitherDecodeFileStrict, toJSON)
 import Data.Aeson.Schema
 import Data.HashMap.Strict (union, fromList)
 import qualified Data.Text as T
--- import Scraper
-    -- ( fetchGameScheduleForDate, hasGamesForDate)
--- import InputSchemas
--- import OutputSchemas
-
-type Boxscore = [schema|
-  {
-    teams: {
-      away: {
-        players: List Player
-      },
-      home: {
-        players: List PLayer
-      }, 
-    }, 
-  }
-|]
-
-type Player = [schema|
-  {
-    person: List {
-      id: Int,
-      fullName: Text,
-    },
-    parentTeamId: Int,
-    allPositions: Maybe List {
-      code: Text,
-    },
-    status: {
-      code: Text,
-    },
-    stats: List {
-        batting: Maybe BattingStats,
-        pitching: Maybe PitchingStats,
-    },
-  }
-|]
-
-type BattingStats = [schema|
-  {
-    gamesPlayed: Int,
-    flyOuts: Int,
-    groundOuts: Int,
-    runs: Int,
-    doubles: Int,
-    triples: Int,
-    homeRuns: Int,
-    strikeOuts: Int,
-    baseOnBalls: Int,
-    intentionalWalks: Int,
-    hits: Int,
-    hitByPitch: Int,
-    atBats: Int,
-    caughtStealing: Int,
-    stolenBases: Int,
-    groundIntoDoublePlay: Int,
-    groundIntoTriplePlay: Int,
-    plateAppearances: Int,
-    totalBases: Int,
-    rbi: Int,
-    leftOnBase: Int,
-    sacBunts: Int,
-    sacFlies: Int,
-    catchersInterference: Int,
-    pickoffs: Int,
-  }
-|]
-
-type PitchingStats = [schema|
-  {
-    gamesPlayed: Int,
-    gamesStarted: Int,
-    groundOuts: Int,
-    airOuts: Int,
-    runs: Int,
-    doubles: Int,
-    triples: Int,
-    homeRuns: Int,
-    strikeOuts: Int,
-    baseOnBalls: Int,
-    intentionalWalks: Int,
-    hits: Int,
-    hitByPitch: Int,
-    atBats: Int,
-    caughtStealing: Int,
-    stolenBases: Int,
-    numberOfPitches: Int,
-    inningsPitched: Text,
-    wins: Int,
-    losses: Int,
-    saves: Int,
-    saveOpportunities: Int,
-    holds: Int,
-    blownSaves: Int,
-    earnedRuns: Int,
-    battersFaced: Int,
-    outs: Int,
-    gamesPitched: Int,
-    completeGames: Int,
-    shutouts: Int,
-    pitchesThrown: Int,
-    balls: Int,
-    strikes: Int,
-    hitBatsmen: Int,
-    balks: Int,
-    wildPitches: Int,
-    pickoffs: Int,
-    gamesFinished: Int,
-    inheritedRunners: Int,
-    inheritedRunnersScored: Int,
-    catchersInterference: Int,
-    sacBunts: Int,
-    sacFlies: Int,
-    passedBall: Int,
-  }
-|]
+import Scraper
+    ( fetchGameScheduleForDate, hasGamesForDate)
+import InputSchemas
+import OutputSchemas
 
 
 main :: IO ()
@@ -133,15 +21,13 @@ main = do
     -- gameSchedule <- fetchGameScheduleForDate "2023-08-22"
     -- print gameSchedule
 
-    -- let hasGames = hasGamesForDate gameSchedule
-    -- print hasGames
-
-  -- Then, load data from a file
   obj <- either fail return =<<
     eitherDecodeFileStrict "testFiles/716896_boxscore.json" :: IO (Object Boxscore)
+  
+  print obj
        
   -- print all the users' ids
   
-  print [get| obj.teams.away.players.stats[] |]
-  print [get| obj.teams.home.players.stats[] |]
+  print [get| obj.teams.away.players[] |]
+  print [get| obj.teams.home.players[] |]
 
