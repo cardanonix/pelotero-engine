76418afe208b118643740860a32c5a157f816fdc
harryprayiv <harryprayiv@gmail.com>
2023-09-05 13:28:51 -0400

    brunout is setting in

--- src/DK_Test.hs
+++ src/DK_Test.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE FlexibleInstances, OverloadedStrings #-}
 
+module Main (main) where
+
 import Data.Aeson
 import Data.Aeson.Types
 import Data.HashMap.Strict
@@ -11,12 +13,20 @@ data Entry = Entry
   }
   deriving Show
 
-instance FromJSON [Entry] where
-  parseJSON x =
-    parseJSON x >>= mapM parseEntry . toList
+newtype Entries = Entries [Entry] deriving Show
+
+instance FromJSON Entries where
+  parseJSON x = Entries <$> (parseJSON x >>= mapM parseEntry . toList)
 
 parseEntry :: (String, Value) -> Parser Entry
 parseEntry (i, v) =
   withObject "entry body" (\ o ->
     Entry i <$> o .: "name" <*> o .: "location")
     v
+
+main :: IO ()
+main = do
+  result <- eitherDecodeFileStrict "testFiles/testfile2.json" :: IO (Either String Entries)
+  case result of
+    Left err -> putStrLn $ "Error decoding file: " ++ err
+    Right entries -> print entries
