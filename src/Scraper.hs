04ab7c60615d3452efbc6a4e70ab5ccad79f1d12
harryprayiv <harryprayiv@gmail.com>
2023-09-07 10:10:22 -0400

    project cleanup to prepare to move on

---
+++ src/Scraper.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main (main) where
+
+
+import Data.Text (Text)
+import qualified Data.Text as Text
+import qualified Data.Map.Strict as M
+import qualified Data.ByteString as B
+import Data.ByteString ( ByteString)
+import Data.ByteString.Lazy.Char8 (pack)
+import qualified Data.ByteString as B (readFile)
+import Data.Aeson (decode, Result(Success), FromJSON(..), Value, (.:), (.:?), (.!=), fromJSON, withObject, eitherDecodeStrict)
+import Data.Aeson.Types (Parser, Result(..))
+import Control.Monad (filterM)
+import Data.Maybe (catMaybes)
+import Debug.Trace (traceShowM)
+import InputADT
+
+main :: IO ()
+main = do
+    jsonData <- B.readFile "testFiles/716896_boxscore.json"
+    let parsedResult = eitherDecodeStrict jsonData :: Either String GameData
+    case parsedResult of
+        Left err -> putStrLn $ "Failed to parse JSON: " ++ err
+        Right gameData -> print gameData
+    handPicked <- B.readFile "testFiles/shortened.json"
+    let parsedResult = eitherDecodeStrict handPicked :: Either String GameData
+    case parsedResult of
+        Left err -> putStrLn $ "Failed to parse JSON: " ++ err
+        Right gameData -> print gameData
