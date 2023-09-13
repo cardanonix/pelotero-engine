33f516818874cb5621d34f9b1c23c873732926b4
harryprayiv <harryprayiv@gmail.com>
2023-09-12 20:10:54 -0400

    polishing a turd

--- src/InputADT.hs
+++ src/InputADT.hs
@@ -65,18 +65,150 @@ data GameData = GameData
   }
   deriving (Show, Eq)
 
+
+-- Teams data type
+data Teams where
+  Teams :: {away :: TeamData, home :: TeamData} -> Teams
+  deriving (Show, Eq)
+
+data TeamData where
+  TeamData :: {players :: M.Map Text Player} -> TeamData
+  deriving (Show, Eq)
+
+hasValidPositions :: Value -> Bool
+hasValidPositions val = case fromJSON val :: Result Player of
+  Success player -> case allPositions player of
+    Just positions -> not (null positions)
+    Nothing -> False
+  _ -> False
+
+type Players = [(Text, Player)]
+
+-- Player data structure
+data Player where
+  Player :: {person :: Person,
+               parentTeamId :: Int,
+               allPositions :: Maybe [Position],
+               status :: Status,
+               stats :: PlayerStats}
+              -> Player
+  deriving (Show, Eq)
+
+data Person where
+  Person :: {personId :: Int, fullName :: Text} -> Person
+  deriving (Show, Eq)
+
+data Position where
+  Position :: {pos_code :: Text} -> Position
+  deriving (Show, Eq)
+
+data Status where
+  Status :: {status_code :: Text} -> Status
+  deriving (Show, Eq)
+
+data PlayerStats where
+  PlayerStats :: {batting :: Maybe BattingStats,
+                    pitching :: Maybe PitchingStats}
+                   -> PlayerStats
+  deriving (Show, Eq)
+
+data BattingStats where
+  BattingStats :: {bat_gamesPlayed :: Maybe Int,
+                     bat_flyOuts :: Maybe Int,
+                     bat_groundOuts :: Maybe Int,
+                     bat_runs :: Maybe Int,
+                     bat_doubles :: Maybe Int,
+                     bat_triples :: Maybe Int,
+                     bat_homeRuns :: Maybe Int,
+                     bat_strikeOuts :: Maybe Int,
+                     bat_baseOnBalls :: Maybe Int,
+                     bat_intentionalWalks :: Maybe Int,
+                     bat_hits :: Maybe Int,
+                     bat_hitByPitch :: Maybe Int,
+                     bat_atBats :: Maybe Int,
+                     bat_caughtStealing :: Maybe Int,
+                     bat_stolenBases :: Maybe Int,
+                     bat_groundIntoDoublePlay :: Maybe Int,
+                     bat_groundIntoTriplePlay :: Maybe Int,
+                     bat_plateAppearances :: Maybe Int,
+                     bat_totalBases :: Maybe Int,
+                     bat_rbi :: Maybe Int,
+                     bat_leftOnBase :: Maybe Int,
+                     bat_sacBunts :: Maybe Int,
+                     bat_sacFlies :: Maybe Int,
+                     bat_catchersInterference :: Maybe Int,
+                     bat_pickoffs :: Maybe Int}
+                    -> BattingStats
+  deriving (Show, Eq)
+
+data PitchingStats where
+  PitchingStats :: {pit_gamesPlayed :: Maybe Int,
+                      pit_gamesStarted :: Maybe Int,
+                      pit_flyOuts :: Maybe Int,
+                      pit_groundOuts :: Maybe Int,
+                      pit_airOuts :: Maybe Int,
+                      pit_runs :: Maybe Int,
+                      pit_doubles :: Maybe Int,
+                      pit_triples :: Maybe Int,
+                      pit_homeRuns :: Maybe Int,
+                      pit_strikeOuts :: Maybe Int,
+                      pit_baseOnBalls :: Maybe Int,
+                      pit_intentionalWalks :: Maybe Int,
+                      pit_hits :: Maybe Int,
+                      pit_hitByPitch :: Maybe Int,
+                      pit_atBats :: Maybe Int,
+                      pit_caughtStealing :: Maybe Int,
+                      pit_stolenBases :: Maybe Int,
+                      pit_numberOfPitches :: Maybe Int,
+                      pit_inningsPitched :: Maybe Text,
+                      pit_wins :: Maybe Int,
+                      pit_losses :: Maybe Int,
+                      pit_saves :: Maybe Int,
+                      pit_saveOpportunities :: Maybe Int,
+                      pit_holds :: Maybe Int,
+                      pit_blownSaves :: Maybe Int,
+                      pit_earnedRuns :: Maybe Int,
+                      pit_battersFaced :: Maybe Int,
+                      pit_outs :: Maybe Int,
+                      pit_gamesPitched :: Maybe Int,
+                      pit_completeGames :: Maybe Int,
+                      pit_shutouts :: Maybe Int,
+                      pit_pitchesThrown :: Maybe Int,
+                      pit_balls :: Maybe Int,
+                      pit_strikes :: Maybe Int,
+                      pit_hitBatsmen :: Maybe Int,
+                      pit_balks :: Maybe Int,
+                      pit_wildPitches :: Maybe Int,
+                      pit_pickoffs :: Maybe Int,
+                      pit_rbi :: Maybe Int,
+                      pit_gamesFinished :: Maybe Int,
+                      pit_inheritedRunners :: Maybe Int,
+                      pit_inheritedRunnersScored :: Maybe Int,
+                      pit_catchersInterference :: Maybe Int,
+                      pit_sacBunts :: Maybe Int,
+                      pit_sacFlies :: Maybe Int,
+                      pit_passedBall :: Maybe Int}
+                     -> PitchingStats
+  deriving (Show, Eq)
+
+-- ## Schedule ADT's ##
+data GameSchedule where
+  GameSchedule :: {dates :: [DateEntry]} -> GameSchedule
+  deriving (Show, Eq)
+
+data DateEntry where
+  DateEntry :: {games :: Maybe (V.Vector GameID)} -> DateEntry
+  deriving (Show, Eq)
+
+data GameID where
+  GameID :: {gamePk :: Int} -> GameID
+  deriving (Show, Eq)
+
 -- JSON instances
 instance FromJSON GameData where
   parseJSON :: Value -> Parser GameData
   parseJSON = withObject "GameData" $ \v ->
     GameData <$> v .: "teams"
-
--- Teams data type
-data Teams = Teams
-  { away :: TeamData,
-    home :: TeamData
-  }
-  deriving (Show, Eq)
 
 instance FromJSON Teams where
   parseJSON :: Value -> Parser Teams
@@ -86,18 +218,6 @@ instance FromJSON Teams where
       .: "away"
       <*> v
       .: "home"
-
-data TeamData = TeamData
-  { players :: M.Map Text Player
-  }
-  deriving (Show, Eq)
-
-hasValidPositions :: Value -> Bool
-hasValidPositions val = case fromJSON val :: Result Player of
-  Success player -> case allPositions player of
-    Just positions -> not (null positions)
-    Nothing -> False
-  _ -> False
 
 instance FromJSON TeamData where
   parseJSON :: Value -> Parser TeamData
@@ -117,18 +237,6 @@ instance FromJSON TeamData where
     let validPlayers = M.fromList $ catMaybes maybePlayersList
     pure TeamData {players = validPlayers}
 
-type Players = [(Text, Player)]
-
--- Player data structure
-data Player = Player
-  { person :: Person,
-    parentTeamId :: Int,
-    allPositions :: Maybe [Position],
-    status :: Status,
-    stats :: PlayerStats
-  }
-  deriving (Show, Eq)
-
 instance FromJSON Player where
   parseJSON :: Value -> Parser Player
   parseJSON = withObject "Player" $ \v -> do
@@ -142,12 +250,6 @@ instance FromJSON Player where
     stats <- v .: "stats"
     return $ Player person teamId validPositions status stats
 
-data Person = Person
-  { personId :: Int,
-    fullName :: Text
-  }
-  deriving (Show, Eq)
-
 instance FromJSON Person where
   parseJSON :: Value -> Parser Person
   parseJSON = withObject "Person" $ \v ->
@@ -157,22 +259,12 @@ instance FromJSON Person where
       <*> v
       .: "fullName"
 
-data Position = Position
-  { pos_code :: Text
-  }
-  deriving (Show, Eq)
-
 instance FromJSON Position where
   parseJSON :: Value -> Parser Position
   parseJSON = withObject "Position" $ \v ->
     Position
       <$> v
       .: "code"
-
-data Status = Status
-  { status_code :: Text
-  }
-  deriving (Show, Eq)
 
 instance FromJSON Status where
   parseJSON :: Value -> Parser Status
@@ -181,47 +273,12 @@ instance FromJSON Status where
       <$> v
       .: "code"
 
-data PlayerStats = PlayerStats
-  { batting :: Maybe BattingStats,
-    pitching :: Maybe PitchingStats
-  }
-  deriving (Show, Eq)
-
 instance FromJSON PlayerStats where
   parseJSON :: Value -> Parser PlayerStats
   parseJSON = withObject "PlayerStats" $ \v ->
     PlayerStats
       <$> v .:? "batting"
       <*> v .:? "pitching"
-
-data BattingStats = BattingStats
-  { bat_gamesPlayed :: Maybe Int,
-    bat_flyOuts :: Maybe Int,
-    bat_groundOuts :: Maybe Int,
-    bat_runs :: Maybe Int,
-    bat_doubles :: Maybe Int,
-    bat_triples :: Maybe Int,
-    bat_homeRuns :: Maybe Int,
-    bat_strikeOuts :: Maybe Int,
-    bat_baseOnBalls :: Maybe Int,
-    bat_intentionalWalks :: Maybe Int,
-    bat_hits :: Maybe Int,
-    bat_hitByPitch :: Maybe Int,
-    bat_atBats :: Maybe Int,
-    bat_caughtStealing :: Maybe Int,
-    bat_stolenBases :: Maybe Int,
-    bat_groundIntoDoublePlay :: Maybe Int,
-    bat_groundIntoTriplePlay :: Maybe Int,
-    bat_plateAppearances :: Maybe Int,
-    bat_totalBases :: Maybe Int,
-    bat_rbi :: Maybe Int,
-    bat_leftOnBase :: Maybe Int,
-    bat_sacBunts :: Maybe Int,
-    bat_sacFlies :: Maybe Int,
-    bat_catchersInterference :: Maybe Int,
-    bat_pickoffs :: Maybe Int
-  }
-  deriving (Show, Eq)
 
 instance FromJSON BattingStats where
   parseJSON :: Value -> Parser BattingStats
@@ -252,56 +309,6 @@ instance FromJSON BattingStats where
       <*> v .:? "sacFlies"
       <*> v .:? "catchersInterference"
       <*> v .:? "pickoffs"
-
-data PitchingStats = PitchingStats
-  { pit_gamesPlayed :: Maybe Int,
-    pit_gamesStarted :: Maybe Int,
-    pit_flyOuts :: Maybe Int,
-    pit_groundOuts :: Maybe Int,
-    pit_airOuts :: Maybe Int,
-    pit_runs :: Maybe Int,
-    pit_doubles :: Maybe Int,
-    pit_triples :: Maybe Int,
-    pit_homeRuns :: Maybe Int,
-    pit_strikeOuts :: Maybe Int,
-    pit_baseOnBalls :: Maybe Int,
-    pit_intentionalWalks :: Maybe Int,
-    pit_hits :: Maybe Int,
-    pit_hitByPitch :: Maybe Int,
-    pit_atBats :: Maybe Int,
-    pit_caughtStealing :: Maybe Int,
-    pit_stolenBases :: Maybe Int,
-    pit_numberOfPitches :: Maybe Int,
-    pit_inningsPitched :: Maybe Text,
-    pit_wins :: Maybe Int,
-    pit_losses :: Maybe Int,
-    pit_saves :: Maybe Int,
-    pit_saveOpportunities :: Maybe Int,
-    pit_holds :: Maybe Int,
-    pit_blownSaves :: Maybe Int,
-    pit_earnedRuns :: Maybe Int,
-    pit_battersFaced :: Maybe Int,
-    pit_outs :: Maybe Int,
-    pit_gamesPitched :: Maybe Int,
-    pit_completeGames :: Maybe Int,
-    pit_shutouts :: Maybe Int,
-    pit_pitchesThrown :: Maybe Int,
-    pit_balls :: Maybe Int,
-    pit_strikes :: Maybe Int,
-    pit_hitBatsmen :: Maybe Int,
-    pit_balks :: Maybe Int,
-    pit_wildPitches :: Maybe Int,
-    pit_pickoffs :: Maybe Int,
-    pit_rbi :: Maybe Int,
-    pit_gamesFinished :: Maybe Int,
-    pit_inheritedRunners :: Maybe Int,
-    pit_inheritedRunnersScored :: Maybe Int,
-    pit_catchersInterference :: Maybe Int,
-    pit_sacBunts :: Maybe Int,
-    pit_sacFlies :: Maybe Int,
-    pit_passedBall :: Maybe Int
-  }
-  deriving (Show, Eq)
 
 instance FromJSON PitchingStats where
   parseJSON :: Value -> Parser PitchingStats
@@ -354,19 +361,7 @@ instance FromJSON PitchingStats where
       <*> v .:? "sacFlies"
       <*> v .:? "passedBall"
 
--- ## Schedule ADT's ##
-data GameSchedule where
-  GameSchedule :: {dates :: [DateEntry]} -> GameSchedule
-  deriving (Show, Eq)
-
-data DateEntry where
-  DateEntry :: {games :: Maybe (V.Vector GameID)} -> DateEntry
-  deriving (Show, Eq)
-
-data GameID where
-  GameID :: {gamePk :: Int} -> GameID
-  deriving (Show, Eq)
-
+-- Schedule Instances
 instance Data.Aeson.FromJSON GameSchedule where
     parseJSON :: Value -> Parser GameSchedule
     parseJSON = Data.Aeson.withObject "GameSchedule" $ \v -> GameSchedule
