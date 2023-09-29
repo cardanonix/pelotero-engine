80bbe640f52b78e3c241f12e52be28b0c4a828ef
harryprayiv <harryprayiv@gmail.com>
2023-09-28 21:28:39 -0400

    cleanup

--- src/Playground.hs
+++ src/Playground.hs
@@ -6,7 +6,7 @@
 {-# LANGUAGE DoAndIfThenElse #-}
 
 
-module Playground (main) where
+module Main (main) where
 
 
 import Network.HTTP.Simple
@@ -16,8 +16,8 @@ import Network.HTTP.Simple
 import Data.Time
     ( Day, addDays, diffDays, parseTimeOrError, defaultTimeLocale, formatTime )
 import Data.Time.Clock.POSIX ()
-import Data.Time.Clock
-import Data.Time.Format
+import Data.Time.Clock ()
+import Data.Time.Format ()
 import Data.ByteString (ByteString, empty)
 import qualified Data.Vector as V
 import Data.Maybe (isJust, fromMaybe, mapMaybe, maybeToList)
@@ -84,56 +84,127 @@ main = do
 
 
 
--- A (date String) -> [B] (list of gameIds/GameSchedule)
--- takes a date string "YYYY-MM-DD" and outputs a schedule bytestring of that day schdule
-fetchGameScheduleForDate :: String -> IO (Either String GameSchedule)
-fetchGameScheduleForDate date = do
-    scheduleResult <- fetchAndDecodeJSON (scheduleUrl date)
-    return $ fmap (assignDateToSchedule (T.pack date)) scheduleResult
+-- -- A (date String) -> [B] (list of gameIds/GameSchedule)
+-- -- takes a date string "YYYY-MM-DD" and outputs a schedule bytestring of that day schdule
+-- fetchGameScheduleForDate :: String -> IO (Either String I.GameSchedule)
+-- fetchGameScheduleForDate date = do
+--     scheduleResult <- fetchAndDecodeJSON (scheduleUrl date)
+--     return $ fmap (assignDateToSchedule (T.pack date)) scheduleResult
 
--- B (gameId) -> C (status)
--- takes a gameId and returns IO (Either String LiveGameWrapper)
-fetchGameStatus :: Int -> IO (Either String I.LiveGameWrapper)
-fetchGameStatus gameId = fetchAndDecodeJSON (gameStatusUrl gameId)
+-- -- B (gameId) -> C (status)
+-- -- takes a gameId and returns IO (Either String LiveGameWrapper)
+-- fetchGameStatus :: Int -> IO (Either String I.LiveGameWrapper)
+-- fetchGameStatus gameId = fetchAndDecodeJSON (gameStatusUrl gameId)
 
--- B (gameId) -> C (status) -> D (boxscore)
--- takes a gameId and returns IO (Either String GameData)
-fetchFinishedBxScore :: Int -> IO (Either String (Maybe I.GameData))
-fetchFinishedBxScore gameId = do
-    gameStatusResult <- fetchGameStatus gameId
-    case gameStatusResult of
-        Right gameDataWrapper -> do
-            let liveStatusWrapper = gameData gameDataWrapper
-            let liveStatus = gameStatus liveStatusWrapper
-            if codedGameState liveStatus == "F"
-               then do
-                   boxscoreResult <- fetchAndDecodeJSON (boxScoreUrl gameId)
-                   return $ fmap (Just . assignGameIdToPlayers gameId) boxscoreResult -- *adds gameId attribute to corresponding stats
-               else return $ Right Nothing
-        Left err -> return $ Left ("Error fetching game status: " ++ err)
+-- -- B (gameId) -> C (status) -> D (boxscore)
+-- -- takes a gameId and returns IO (Either String GameData)
+-- fetchFinishedBxScore :: Int -> IO (Either String (Maybe I.GameData))
+-- fetchFinishedBxScore gameId = do
+--     gameStatusResult <- fetchGameStatus gameId
+--     case gameStatusResult of
+--         Right gameDataWrapper -> do
+--             let liveStatusWrapper = gameData gameDataWrapper
+--             let liveStatus = gameStatus liveStatusWrapper
+--             if codedGameState liveStatus == "F"
+--                then do
+--                    boxscoreResult <- fetchAndDecodeJSON (boxScoreUrl gameId)
+--                    return $ fmap (Just . assignGameIdToPlayers gameId) boxscoreResult -- *adds gameId attribute to corresponding stats
+--                else return $ Right Nothing
+--         Left err -> return $ Left ("Error fetching game status: " ++ err)
 
 
--- -- [B] list of gameIds -> C status checks -> [D] list of boxscores
--- fetchFinishedBxScores :: [Int] -> IO (Either String (M.Map Int I.GameData))
-fetchFinishedBxScores :: [Int] -> IO (Either String (M.Map Int (Maybe I.GameData)))
-fetchFinishedBxScores gameIds = do
-    results <- mapConcurrently fetchGame gameIds
-    let combinedResults = sequenceA results -- Change the structure from [Either] to Either [..]
-    return $ fmap (M.fromList . filter finishedGames) combinedResults
-    where 
-        fetchGame gameId = do
-            result <- fetchFinishedBxScore gameId
-            return $ fmap (\d -> (gameId, d)) result
-        finishedGames (_, Nothing) = False
-        finishedGames (_, Just _) = True
+-- -- -- [B] list of gameIds -> C status checks -> [D] list of boxscores
+-- -- fetchFinishedBxScores :: [Int] -> IO (Either String (M.Map Int I.GameData))
+-- fetchFinishedBxScores :: [Int] -> IO (Either String (M.Map Int (Maybe I.GameData)))
+-- fetchFinishedBxScores gameIds = do
+--     results <- mapConcurrently fetchGame gameIds
+--     let combinedResults = sequenceA results -- Change the structure from [Either] to Either [..]
+--     return $ fmap (M.fromList . filter finishedGames) combinedResults
+--     where 
+--         fetchGame gameId = do
+--             result <- fetchFinishedBxScore gameId
+--             return $ fmap (\d -> (gameId, d)) result
+--         finishedGames (_, Nothing) = False
+--         finishedGames (_, Just _) = True
 
--- ## OUTPUT CONVERSION ##
--- [B] list of gameIds -> C status checks -> [D] (list of box scores) -> [E] (list of player data)
-fetchFinishedBxScoresToJsonPlayerData :: [Int] -> IO (Either String (M.Map Text MI.JsonPlayerData))
-fetchFinishedBxScoresToJsonPlayerData gameIds = do
-    gameDataResult <- fetchFinishedBxScores gameIds
-    return $ fmap convertGameDataMapToJsonPlayerData gameDataResult
+-- -- ## OUTPUT CONVERSION ##
+-- -- [B] list of gameIds -> C status checks -> [D] (list of box scores) -> [E] (list of player data)
+-- fetchFinishedBxScoresToJsonPlayerData :: [Int] -> IO (Either String (M.Map Text MI.JsonPlayerData))
+-- fetchFinishedBxScoresToJsonPlayerData gameIds = do
+--     gameDataResult <- fetchFinishedBxScores gameIds
+--     return $ fmap convertGameDataMapToJsonPlayerData gameDataResult
 
+-- -- Fetch and decode utility
+-- fetchAndDecodeJSON :: FromJSON a => String -> IO (Either String a)
+-- fetchAndDecodeJSON url = do
+--     response <- httpBS (parseRequest_ url)
+--     return $ eitherDecodeStrict $ getResponseBody response
+
+-- convertGameDataMapToJsonPlayerData :: M.Map Int (Maybe I.GameData) -> M.Map Text MI.JsonPlayerData
+-- convertGameDataMapToJsonPlayerData maybeGameDataMap =
+--     foldl mergePlayerData M.empty allPlayerDataPairs
+--   where
+--     allPlayerDataPairs :: [(Text, MI.JsonPlayerData)]
+--     allPlayerDataPairs = concatMap gameDataToPlayerDataPairs (mapMaybe id (M.elems maybeGameDataMap))
+
+--     gameDataToPlayerDataPairs :: I.GameData -> [(Text, MI.JsonPlayerData)]
+--     gameDataToPlayerDataPairs gameData =
+--         let awayPlayers = M.elems $ I.players $ I.away $ I.teams gameData
+--             homePlayers = M.elems $ I.players $ I.home $ I.teams gameData
+--             allPlayers = awayPlayers ++ homePlayers
+--         in map (\player -> (rawStringToText $ MI.playerId (playerToJsonPlayerData player), playerToJsonPlayerData player)) allPlayers
+
+--     rawStringToText :: Text -> Text
+--     rawStringToText = T.replace "\\\"" "\"" . T.replace "\\\\" "\\"
+
+--     mergePlayerData :: M.Map Text MI.JsonPlayerData -> (Text, MI.JsonPlayerData) -> M.Map Text MI.JsonPlayerData
+--     mergePlayerData acc (playerId, newPlayerData) =
+--         let mergedData = case M.lookup playerId acc of
+--                 Just existingPlayerData -> mergeJsonPlayerData existingPlayerData newPlayerData
+--                 Nothing                 -> newPlayerData
+--         in M.insert playerId mergedData acc
+
+-- mergeJsonPlayerData :: MI.JsonPlayerData -> MI.JsonPlayerData -> MI.JsonPlayerData
+-- mergeJsonPlayerData existing new = 
+--     MI.JsonPlayerData
+--         { MI.playerId = MI.playerId existing  -- assuming playerIds are the same, else there's a bigger problem!
+--         , MI.fullName = MI.fullName existing  -- assuming fullNames are the same
+--         , MI.stats = M.unionWith mergeJsonStatsData (MI.stats existing) (MI.stats new)
+--         }
+
+-- mergeJsonStatsData :: MI.JsonStatsData -> MI.JsonStatsData -> MI.JsonStatsData
+-- mergeJsonStatsData _ new = new
+
+-- playerToJsonPlayerData :: I.Player -> MI.JsonPlayerData
+-- playerToJsonPlayerData p =
+--     MI.JsonPlayerData
+--         { MI.playerId = T.pack $ show $ I.personId (I.person p)
+--         , MI.fullName = I.fullName (I.person p)
+--         , MI.stats = M.singleton (maybe "" (T.pack . show) (I.gameid p)) (playerToJsonStatsData p)
+--         }
+
+-- playerToJsonStatsData :: I.Player -> MI.JsonStatsData
+-- playerToJsonStatsData p =
+--     MI.JsonStatsData
+--         { MI.parentTeamId = I.parentTeamId p
+--         , MI.allPositions = fromMaybe [] (I.allPositions p)
+--         , MI.statusCode = I.status_code (I.status p)
+--         , MI.batting = I.batting (I.stats p)
+--         , MI.pitching = I.pitching (I.stats p)
+--         }
+
+-- convertPlayerToJson :: I.Player -> ByteString
+-- convertPlayerToJson = BL.toStrict . encode . playerToJsonPlayerData
+
+
+-- assignGameIdToPlayers :: Int -> I.GameData -> I.GameData
+-- assignGameIdToPlayers gameId gameData =
+--     let assignToTeam team = team { I.players = M.map assignToPlayer (players team) }
+--         assignToPlayer player = player { I.gameid = Just gameId }
+--     in gameData { I.teams = (teams gameData) { I.away = assignToTeam (I.away (teams gameData)),
+--                                              I.home = assignToTeam (I.home (teams gameData)) } }
+
+{- 
 -- ## Output Stuff ##
 processDate :: String -> IO ()
 processDate date = do
@@ -212,18 +283,6 @@ assignDateToSchedule date schedule =
         assignToDate gameID = gameID { game_date = Just date }
     in schedule { dates = map assignToDateEntry (dates schedule) }
 
-assignGameIdToPlayers :: Int -> GameData -> GameData
-assignGameIdToPlayers gameId gameData =
-    let assignToTeam team = team { players = M.map assignToPlayer (players team) }
-        assignToPlayer player = player { gameid = Just gameId }
-    in gameData { teams = (teams gameData) { I.away = assignToTeam (I.away (teams gameData)),
-                                             I.home = assignToTeam (I.home (teams gameData)) } }
-
--- Fetch and decode utility
-fetchAndDecodeJSON :: FromJSON a => String -> IO (Either String a)
-fetchAndDecodeJSON url = do
-    response <- httpBS (parseRequest_ url)
-    return $ eitherDecodeStrict $ getResponseBody response
 
 -- Takes a schedule bytestring and outputs true if games are happening, false otherwise.
 hasGamesForDate :: I.GameSchedule -> Bool
@@ -245,62 +304,6 @@ writeDataToFile filename path dataToSave = do
 writePlayerToJsonFile :: FilePath -> I.Player -> IO ()
 writePlayerToJsonFile path player = B.writeFile path (convertPlayerToJson player)
 
--- Stat-Mutation stuff
-playerToJsonPlayerData :: I.Player -> MI.JsonPlayerData
-playerToJsonPlayerData p =
-    MI.JsonPlayerData
-        { MI.playerId = T.pack $ show $ I.personId (I.person p)
-        , MI.fullName = I.fullName (I.person p)
-        , MI.stats = M.singleton (maybe "" (T.pack . show) (I.gameid p)) (playerToJsonStatsData p)
-        }
-
-playerToJsonStatsData :: I.Player -> MI.JsonStatsData
-playerToJsonStatsData p =
-    MI.JsonStatsData
-        { MI.parentTeamId = I.parentTeamId p
-        , MI.allPositions = fromMaybe [] (I.allPositions p)
-        , MI.statusCode = I.status_code (I.status p)
-        , MI.batting = I.batting (I.stats p)
-        , MI.pitching = I.pitching (I.stats p)
-        }
-
-convertPlayerToJson :: I.Player -> ByteString
-convertPlayerToJson = BL.toStrict . encode . playerToJsonPlayerData
-
-convertGameDataMapToJsonPlayerData :: M.Map Int (Maybe I.GameData) -> M.Map Text MI.JsonPlayerData
-convertGameDataMapToJsonPlayerData maybeGameDataMap =
-    foldl mergePlayerData M.empty allPlayerDataPairs
-  where
-    allPlayerDataPairs :: [(Text, MI.JsonPlayerData)]
-    allPlayerDataPairs = concatMap gameDataToPlayerDataPairs (mapMaybe id (M.elems maybeGameDataMap))
-
-    gameDataToPlayerDataPairs :: I.GameData -> [(Text, MI.JsonPlayerData)]
-    gameDataToPlayerDataPairs gameData =
-        let awayPlayers = M.elems $ I.players $ I.away $ I.teams gameData
-            homePlayers = M.elems $ I.players $ I.home $ I.teams gameData
-            allPlayers = awayPlayers ++ homePlayers
-        in map (\player -> (rawStringToText $ MI.playerId (playerToJsonPlayerData player), playerToJsonPlayerData player)) allPlayers
-
-    rawStringToText :: Text -> Text
-    rawStringToText = T.replace "\\\"" "\"" . T.replace "\\\\" "\\"
-
-    mergePlayerData :: M.Map Text MI.JsonPlayerData -> (Text, MI.JsonPlayerData) -> M.Map Text MI.JsonPlayerData
-    mergePlayerData acc (playerId, newPlayerData) =
-        let mergedData = case M.lookup playerId acc of
-                Just existingPlayerData -> mergeJsonPlayerData existingPlayerData newPlayerData
-                Nothing                 -> newPlayerData
-        in M.insert playerId mergedData acc
-
-mergeJsonPlayerData :: MI.JsonPlayerData -> MI.JsonPlayerData -> MI.JsonPlayerData
-mergeJsonPlayerData existing new = 
-    MI.JsonPlayerData
-        { MI.playerId = MI.playerId existing  -- assuming playerIds are the same, else there's a bigger problem!
-        , MI.fullName = MI.fullName existing  -- assuming fullNames are the same
-        , MI.stats = M.unionWith mergeJsonStatsData (MI.stats existing) (MI.stats new)
-        }
-
-mergeJsonStatsData :: MI.JsonStatsData -> MI.JsonStatsData -> MI.JsonStatsData
-mergeJsonStatsData _ new = new
 
 -- Takes a date string and formats it as a filename, like "2023_08_22.json".
 formatFilename :: String -> String
@@ -355,3 +358,4 @@ computeChecksum bs = T.pack . show . hashWith SHA256 $ BL.toStrict bs
 getCurrentDate :: IO Text
 getCurrentDate = T.pack . formatTime defaultTimeLocale "%Y_%m_%d_%H_%M" <$> getCurrentTime
 
+ -}
