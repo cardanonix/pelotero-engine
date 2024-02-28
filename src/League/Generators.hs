034bc4357d11a44bccf5c38d2d7379a01b1003d1
harryprayiv <harryprayiv@gmail.com>
2024-02-28 08:30:28 -0500

    create random roster generator

--- src/League/Generators.hs
+++ src/League/Generators.hs
@@ -1,44 +1,64 @@
--- {-# LANGUAGE OverloadedStrings #-}
--- {-# LANGUAGE RecordWildCards #-}
 
 module Main (main) where
 
-import Control.Monad (filterM, forM)
-import Data.Aeson (FromJSON (..), Result (Success), ToJSON (..), Value, decode, eitherDecodeStrict, fromJSON, withObject, (.!=), (.:), (.:?))
-import Data.Aeson.Types (Parser, Result (..))
-import Data.ByteString (ByteString)
-import Data.List ( find, delete, nub, (\\) )
-import qualified Data.ByteString as B
-import Data.ByteString.Lazy.Char8 (pack)
-import Data.Foldable (foldl', forM_)
-import qualified Data.Map.Strict as M
-import Data.Maybe (catMaybes, mapMaybe)
-import Data.Text (Text)
-import qualified Data.Text as Text
-import Debug.Trace (traceShow, traceShowM)
+import Control.Monad ( filterM, forM, forM_)
+
 import System.Random (randomR, newStdGen, StdGen)
-
-
-
-
+import Data.List (delete)
+import Data.Text (Text)
+import qualified Data.Text as T
+import Data.Time.Clock (UTCTime, getCurrentTime)
+import GHC.Generics (Generic)
+import Data.Aeson (FromJSON, ToJSON, encode, parseJSON, withObject, (.:))
+import qualified Data.HashMap.Strict as HM
 import qualified Config as C
-import qualified GHC.Generics as R
 import qualified Input as I
 import OfficialRoster as O
 import qualified OfficialRoster as O
 import qualified Points as P
 import qualified Roster as R
+import qualified Ranking as PR
 import Validators
+import Utility
+    ( readJson, writeJson, computeChecksum, shuffleList )
 
--- pure function to generate a random number (and a new generator)
-randomIntGen :: (Int, Int) -> StdGen -> (Int, StdGen)
-randomIntGen range gen = randomR range gen
 
-randomInt :: (Int, Int) -> StdGen -> (Int, StdGen)
-randomInt = randomR
+-- Updated createRandomRankings function
+createRandomRankings :: O.OfficialRoster -> IO [PR.PlayerRanking]
+createRandomRankings officialRoster = do
+  gen <- newStdGen
+  let players = O.people officialRoster
+  let (shuffledPlayers, _) = shuffleList players gen
+  let rankings = zipWith (\rank player -> PR.PlayerRanking (O.playerId player) rank) [1..] shuffledPlayers
+  return rankings
+
+
 
 main :: IO ()
 main = do
-    gen <- newStdGen
-    let (randomNumber, newGen) = randomInt (1, 1226) gen
-    print randomNumber
+    eitherRoster <- readJson "testFiles/appData/rosters/activePlayers.json"
+    case eitherRoster of
+        Right roster -> do
+            rankings <- createRandomRankings roster
+            putStrLn "Randomly generated player rankings:"
+            forM_ rankings $ \(PR.PlayerRanking playerId rank) ->
+                putStrLn $ "Player ID: " ++ show playerId ++ ", Rank: " ++ show rank
+
+            -- Generate the necessary values for RankingData
+            currentTime <- getCurrentTime
+            let teamId = T.pack "yourTeamId"  -- Convert String to Text
+
+            -- Serialize rankingData to JSON for checksum calculation
+            let rankingData = PR.RankingData teamId (T.pack "") currentTime rankings  -- Temporarily empty checksum, converted to Text
+            let rankingDataJson = encode rankingData
+            let dataChecksum = computeChecksum rankingDataJson
+
+            -- Update rankingData with actual checksum
+            let rankingDataWithChecksum = rankingData { PR.dataChecksum = dataChecksum }
+
+            -- Write the ranking data to a file
+            writeJson "testFiles/appData/rankings/randomRankings.json" rankingDataWithChecksum
+
+            putStrLn "Rankings written to JSON file successfully."
+        Left error ->
+            putStrLn $ "Failed to load the roster: " ++ error
